edca7bcd3e44a0fcbce2e8f8e61ea49df417a059
kawata-yuya <kawatayuya@icloud.com>
2023-12-03 23:08:31 +0900

    add cliのコメントアウトを追加

--- pwmfft/cli.py
+++ pwmfft/cli.py
@@ -31,20 +31,26 @@ def main() -> None:
             "filepath": filepath,
             "filename": path.splitext(path.basename(filepath))[0],
         }
-                        
-        oscillo_dft.read_csv(oscillo_data_info['filepath'])                
+        
+        # ASCII形式のデータを読み込み
+        oscillo_dft.read_csv(oscillo_data_info['filepath'])
+        # 読み込んだデータをDFT変換
         oscillo_dft.dft()
         
+        # 電圧波形を出力
         plot_waveform(
             oscillo_dft,
             path.join(output_file_path, f"01出力電圧波形_{oscillo_data_info['filename']}.png")
         )
         
+        # 周波数含有率のグラフを出力
         plot_frequency_contents(
             oscillo_dft,
             path.join(output_file_path, f"02高調波含有率_{oscillo_data_info['filename']}.png"),
             FUNDAMENTAL_FREQUENCY,
         )
+        
+        # 考察用のスペクトラムを出力
         plot_spectrum(
             oscillo_dft,
             path.join(output_file_path, f"03スペクトラム(小)_{oscillo_data_info['filename']}.png"),
@@ -53,6 +59,7 @@ def main() -> None:
             title=oscillo_data_info['filename'],
         )
 
+        # 考察用のスペクトラムを出力
         plot_spectrum(
             oscillo_dft,
             path.join(output_file_path, f"04スペクトラム(大)_{oscillo_data_info['filename']}.png"),
@@ -61,6 +68,7 @@ def main() -> None:
             title=oscillo_data_info['filename'],
         )
         
+        # DFT変換の結果をcsvファイルに保存
         oscillo_dft.save_dft_real_result(path.join(output_file_path, f"05dft結果_{oscillo_data_info['filename']}.csv"),)
         oscillo_dft.save_frequency_contents_result(
             filepath=path.join(output_file_path, f"06高調波含有率_結果_{oscillo_data_info['filename']}.csv"),
@@ -69,8 +77,10 @@ def main() -> None:
             insert_invalid_contents=False
         )
         
+        # 歪み率の表示
         print("歪み率[%]")
-        print(f"{oscillo_data_info['filename']}: {oscillo_dft.get_total_harmonic_distribution(50)}%")
+        total_harmonic_distribution = oscillo_dft.get_total_harmonic_distribution(FUNDAMENTAL_FREQUENCY)
+        print(f"{oscillo_data_info['filename']}: {total_harmonic_distribution}%")
     
 
     return
