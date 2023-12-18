c4be74347c0a01cbdf5fe21a33f4e6d6a122e993
kawata-yuya <kawatayuya@icloud.com>
2023-12-18 14:05:59 +0900

    add コンソールスクリプトの追加

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def requirements_from_file(file_name):
 
 setup(
     name='pwmfft',
-    version='0.0.0',
+    version='0.0.1',
     author="kawawta-yuya",
     description="オシロスコープの波形をDFT変換し管理する",
     url="https://github.com/kawata-yuya/pwmfft",
@@ -16,6 +16,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    entry_points={
+        'console_scripts':[
+            'pwmfft = pwmfft.cli:main',
+        ],
+    },
     python_requires='>=3.8',
     install_requires=requirements_from_file('requirements.txt'),
 )
